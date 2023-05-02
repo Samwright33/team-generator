3eeed0618188adb4a6b90df2366be9d6a02e8525
Sam Wright <samwright@Sams-MacBook-Pro-3.local>
2023-05-01 22:03:28 -0500

    Added intern, manager, and engineer extensions to employee and exports

--- library/manager.js
+++ library/manager.js
@@ -1,1 +1,21 @@
-const Employee = require("./Employee");
+const Employee = require("./employee");
+
+
+class Manager extends Employee {
+    constructor (name, id, email, role, officeNumber) {
+        super (name, id, email);
+        this.officeNumber = officeNumber;
+        this.role = role;
+    }
+
+getOfficeNumber () {
+    return this.officeNumber
+}
+
+getRole () {
+    return "Manager"
+}
+
+};
+
+module.exports = Manager;
